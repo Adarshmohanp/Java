fb85aa22918daf103e5d14c6b5df011890bff24b
Adarsh Mohan P <adarshmohanelayad@gmail.com>
2026-06-10 09:13:14 +0530

    Synchronization with Anonymous class

--- AnonymousSynchro21.java
+++ AnonymousSynchro21.java
@@ -1,5 +1,5 @@
 public class AnonymousSynchro21{
-    public void printCapitals(){
+    public synchronized void  printCapitals(){
         for(int i=65;i<=90;i++){
             System.out.println((char)i+" ");
             try{
@@ -10,7 +10,7 @@ public class AnonymousSynchro21{
         }
     }
 
-    public void printSmaller(){
+    public synchronized void printSmaller(){
         for(int i=97;i<=122;i++){
             System.out.println((char)i+" ");
             try{
